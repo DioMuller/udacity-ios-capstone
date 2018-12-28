6985356aa25053e2928481261ccb025efc2fdcc8
Diogo Muller <diogo.muller@live.com>
2018-12-28 11:33:10 -0200

    Created Project Structure.

---
+++ Itch/Controllers/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Itch
+//
+//  Created by Diogo Muller on 28/12/18.
+//  Copyright © 2018 Diogo Muller. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
