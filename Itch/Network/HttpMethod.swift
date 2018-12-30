bc85c8b9f051ddd2da7b7f039d75d0f63381012e
Diogo Muller <diogo.muller@live.com>
2018-12-30 10:32:38 -0200

    Created Network Communication classes.

---
+++ Itch/Network/HttpMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HttpMethod.swift
+//  Itch
+//
+//  Created by Diogo Muller on 30/12/18.
+//  Copyright © 2018 Diogo Muller. All rights reserved.
+//
+
+import Foundation
+
+enum HttpMethod : String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
---
+++ Itch/Network/HttpResult.swift
@@ -0,0 +1,11 @@
+//
+//  HttpResult.swift
+//  Itch
+//
+//  Created by Diogo Muller on 18/11/18.
+//  Copyright © 2018 Diogo Muller. All rights reserved.
+//
+
+import Foundation
+
+typealias HttpResult<T> = (_ result : T?, _ error : Error? ) -> Void where T : Decodable
