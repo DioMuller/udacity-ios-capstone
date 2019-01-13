3e79750a745126f33095efeb93b4019462417ecd
Diogo Muller <diogo.muller@live.com>
2019-01-13 12:33:58 -0200

    Created Project and it's structure.

---
+++ GameCollector/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameCollector
+//
+//  Created by Diogo Muller on 13/01/19.
+//  Copyright © 2019 Diogo Muller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
