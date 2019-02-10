6bf79c49affe23f7a0dbebaabc1ce0c4ce2118a3
Diogo Muller <diogo.muller@live.com>
2019-02-10 18:24:13 -0200

    Added previously unversioned keys file

---
+++ GameCollector/Constants+Keys.swift
@@ -0,0 +1,15 @@
+//
+//  Constants+Keys.swift
+//  GameCollector
+//
+//  Created by Diogo Muller on 16/01/19.
+//  Copyright © 2019 Diogo Muller. All rights reserved.
+//
+
+import Foundation
+
+extension Constants {
+    class Keys {
+        static let apiKey = "d2a9f1e845bb09ea80e52f333543d99b"
+    }
+}
